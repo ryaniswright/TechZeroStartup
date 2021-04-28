e0da209a44dfa36a53ce9ff40ad760e1a70fa6c5
Ubuntu <ubuntu@ip-172-26-5-167.ec2.internal>
2021-04-28 22:49:14 +0000

    Setting up AWS Lightsail server

--- swift.py
+++ swift.py
@@ -260,6 +260,6 @@ if PYTHONANYWHERE:
     application = default_app()
 else:
    if __name__ == "__main__":
-       run(host='localhost', port=8080, debug=True)
+       run(host='0.0.0.0', port=8000, debug=True)
 
 
